e5dc973e17a19bad363baafd0658db1aca35dc2c
8490 <ethan@clarusway.com>
2021-12-28 21:56:50 +0300

    add UseStateExample.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import UseStateExample from "./UseStateExample";
+
+function App() {
+  return (
+    <div>
+      <h1>USE REDUCER APP</h1>
+
+      <UseStateExample />
+    </div>
+  );
+}
+
+export default App;
